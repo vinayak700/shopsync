84257818f832d3ae54e27737f637448af57784f4
Vinayak Gupta <75876908+vinayak700@users.noreply.github.com>
2024-01-08 19:01:16 +0530

    2`

--- src/config/firebaseInit.jsx
+++ src/config/firebaseInit.jsx
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
@@ -21,7 +20,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 
 // Firestore instance
 const firebaseAuth = getAuth(app);
